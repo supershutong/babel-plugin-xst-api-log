475f8a964d1798cb781c0673e0eaa6bd1f5019df
lixyang <lixyang@yonyou.com>
2022-11-20 01:01:38 +0800

    fix: <DatePicker.WeekPicker locale='en'/>场景的组件及API获取

--- demo/demoFile.js
+++ demo/demoFile.js
@@ -1,23 +1,50 @@
 import * as React from 'react'
-import {ConfigProvider, DatePicker} from '@tinper/next-ui'
+import {Input} from 'antd'
+import {ConfigProvider, Input as FormControl, DatePicker, Checkbox} from '@tinper/next-ui'
+import BeeButton from './Button'
+import '@tinper/next-ui/dist/tinper-next.css'
 
+const CheckboxGroup = Checkbox.Group
 const {RangePicker: Range} = DatePicker
 const registerTheme = ConfigProvider.registerTheme
 
 export default class Page extends React.Component {
-    constructor() {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: ['2', '4']
+        }
         registerTheme('blue')
+        ConfigProvider.config({locale: 'zh-cn'})
+    }
+
+    onChange = (d, dateString) => {
+        console.warn('change--->', d, dateString)
+        this.setState({value: d})
     }
 
     render() {
         return (
-            <>
+            <div className='demoFile'>
+                <Input defaultValue='我是输入框' />
+                <FormControl type='search' />
                 <DatePicker
                     picker='date'
                     showToday
+                    showTime={{showSecond: false, format: 'H点m分'}}
+                    onChange={this.onChange}
                 />
+                <DatePicker.WeekPicker mode='week'/>
                 <Range placeholder={['开始日期', '结束日期']} />
-            </>
+                <CheckboxGroup style={{display: 'inline-block'}} value={this.state.value} readOnly>
+                    <Checkbox value='2'>2</Checkbox>
+                    <Checkbox readOnly={false} value='3'>
+                        3
+                    </Checkbox>
+                    <Checkbox value='4'>4</Checkbox>
+                </CheckboxGroup>
+                <BeeButton type='primary'>提交</BeeButton>
+            </div>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -17,33 +17,18 @@ const colors = require('colors')
 //     'tinper-bee': {Button: {local: 'Button', api: {}}}
 // }
 
+const result = {
+    componentsInCurrentFile: {}
+}
+
 module.exports = ({types: t}, opts) => {
     const {libs = ['@tinper/next-ui', 'tinper-bee'], output = './apiLog.json'} = opts
 
     return {
         name: 'xst-api-log',
         pre(state) {
-            this.result = {}
-            // this.deps = {}
-            this.filePath = path.resolve(__dirname, output)
-            if (!fs.existsSync(this.filePath)) {
-                console.log(colors.green('[API log] 初始化------'))
-            } else {
-                // madge(this.filename).then( async res => {
-                //     this.deps = res.obj()
-                //     console.log(colors.green('[API log] 资源依赖获取------'))
-                //     // console.log(colors.green('[API log] 资源依赖获取------', this.deps))
-                // })
-                // const data = fs.readFileSync(this.filePath, 'utf8')
-                // console.log(colors.green('[API log] 历史纪录------'))
-                // // console.log(colors.green('[API log] 历史纪录------', this.deps, data))
-                // this.result = data
-                //     ? {...JSON.parse(data), componentsInCurrentFile: {deps: this.deps}}
-                //     : {deps: this.deps}
-                const data = fs.readFileSync(this.filePath, 'utf8')
-                this.result = data ? {...JSON.parse(data), componentsInCurrentFile: {}} : {}
-                console.log(colors.green('[API log] json读取成功'))
-            }
+            result.componentsInCurrentFile = {}
+            console.log(colors.green('[API log] 正在解析：', state.opts.sourceFileName))
         },
         visitor: {
             Identifier(path, state) {
@@ -53,7 +38,7 @@ module.exports = ({types: t}, opts) => {
                  * 3、ConfigProvider.registerTheme('blue') 或 解析出实例方法调用 registerTheme('blue')
                  */
                 const parentComp = path.parent.object?.name
-                const libObj = this.result.componentsInCurrentFile?.[parentComp]
+                const libObj = result.componentsInCurrentFile?.[parentComp]
                 if (parentComp && libObj) {
                     const subCompName = path.parent.property.name
                     const local =
@@ -61,19 +46,22 @@ module.exports = ({types: t}, opts) => {
                         path.parentPath.container.id?.name /* Range */ ||
                         path.node.name
 
-                    if (this.result.componentsInCurrentFile[parentComp]) {
-                        // 组件实例上挂载方法需放进组件API内，不能单独声明为组件。如：ConfigProvider.registerTheme('blue')
-                        if (this.result.componentsInCurrentFile[path.node.name]) return // ast多解析出的父节点忽略
+                    // 组件实例上挂载方法需放进组件API内，不能单独声明为组件。如：ConfigProvider.registerTheme('blue')
+                    if (
+                        result.componentsInCurrentFile[parentComp] &&
+                        !/^[A-Z]/.test(subCompName) /** 非大驼峰，避免子组件进入 */
+                    ) {
+                        if (result.componentsInCurrentFile[path.node.name]) return // ast多解析出的父节点忽略
                         // 实例方法计入API
-                        let api = this.result[libObj.lib][parentComp]?.api || {}
+                        let api = result[libObj.lib][parentComp]?.api || {}
                         api[path.node.name] = path.node.type
-                        this.result[libObj.lib][parentComp].api = api
+                        result[libObj.lib][parentComp].api = api
                         return
                     }
-                    this.result.componentsInCurrentFile[local] = {lib: libObj.lib}
-                    this.result[libObj.lib][subCompName] = {
+                    result.componentsInCurrentFile[local] = {lib: libObj.lib}
+                    result[libObj.lib][subCompName] = {
                         local,
-                        api: this.result[libObj.lib][subCompName]?.api || {}
+                        api: result[libObj.lib][subCompName]?.api || {}
                     }
                 }
             },
@@ -81,8 +69,8 @@ module.exports = ({types: t}, opts) => {
                 const node = path.node
                 if (libs.includes(node.source.value)) {
                     // 建仓库
-                    if (!this.result[node.source.value]) {
-                        this.result[node.source.value] = {}
+                    if (!result[node.source.value]) {
+                        result[node.source.value] = {}
                     }
                 }
             },
@@ -90,31 +78,53 @@ module.exports = ({types: t}, opts) => {
                 const node = path.node
                 // 来源于目标仓库的组件，写入组件名、别名
                 if (libs.includes(path.parent.source.value)) {
-                    this.result[path.parent.source.value][node.imported.name] = {
+                    result[path.parent.source.value][node.imported.name] = {
                         local: node.local.name,
-                        api: {}
+                        api: result[path.parent.source.value][node.imported.name]?.api || {} /** 已录入API不能漏 */
                     }
-                    this.result.componentsInCurrentFile = this.result.componentsInCurrentFile || {}
-                    this.result.componentsInCurrentFile[node.local.name] = {lib: path.parent.source.value} // 组件所属框架
+                    result.componentsInCurrentFile = result.componentsInCurrentFile || {}
+                    result.componentsInCurrentFile[node.local.name] = {lib: path.parent.source.value} // 组件所属框架
+                }
+            },
+            JSXIdentifier(path) {
+                const parentComp = path.parent.object?.name
+                const libObj = result.componentsInCurrentFile?.[parentComp]
+                if (parentComp && libObj) {
+                    /** 使用子组件做openingElement标签 DatePicker.WeekPicker */
+                    const subCompName = path.parent.property.name
+                    const local =
+                        path.parentPath.parentPath.node.id?.name /* CheckboxGroup */ ||
+                        path.parentPath.container.id?.name /* Range */ ||
+                        path.node.name
+
+                    result.componentsInCurrentFile[local] = {lib: libObj.lib}
+                    result[libObj.lib][subCompName] = {
+                        local: subCompName,
+                        api: result[libObj.lib][subCompName]?.api || {}
+                    }
                 }
             },
             // 写属性API
             JSXAttribute(path) {
                 const node = path.node
+                const nestComp =
+                    path.parentPath.parentPath.node?.openingElement.name?.property
+                        ?.name /** 使用子组件做openingElement标签 DatePicker.WeekPicker */
                 // 写入Tinper-next和Tinper-bee组件API
-                const currentComp = this.result.componentsInCurrentFile?.[path.parent.name.name]
+                const openingElement = path.parent.name.name /** 使用组件做openingElement标签 DatePicker */ || nestComp
+                const currentComp = result.componentsInCurrentFile?.[openingElement]
                 if (currentComp /* 目标组件 */) {
-                    Object.keys(this.result[currentComp.lib]).forEach((comp, i) => {
-                        if (this.result[currentComp.lib][comp].local === path.parent.name.name) {
+                    Object.keys(result[currentComp.lib]).forEach((comp, i) => {
+                        if (result[currentComp.lib][comp].local === openingElement) {
                             // 使用local，不能直接使用Key，防止同时使用含有相同模块的多个仓库问题，local也处理类似 Input as FormControl用法问题
                             if (node.value === null) {
                                 // boolean默认true不写，如：<DatePicker showToday />
-                                this.result[currentComp.lib][comp].api[node.name.name] = 'BooleanLiteral'
+                                result[currentComp.lib][comp].api[node.name.name] = 'BooleanLiteral'
                             } else if (node.value?.type === 'JSXExpressionContainer') {
                                 // 值为组件/回调
-                                this.result[currentComp.lib][comp].api[node.name.name] = node.value.expression.type
+                                result[currentComp.lib][comp].api[node.name.name] = node.value.expression.type
                             } else {
-                                this.result[currentComp.lib][comp].api[node.name.name] =
+                                result[currentComp.lib][comp].api[node.name.name] =
                                     node.value?.type /* 对象、回调、数组、字符串、bool、空、undefined、number，从property/propertites/elements取值 */
                             }
                         }
@@ -123,9 +133,9 @@ module.exports = ({types: t}, opts) => {
             }
         },
         post(state) {
-            this.result = JSON.stringify(this.result, null, 4)
-            fs.writeFileSync(this.filePath, this.result, {flag: 'w+'})
-            console.log(colors.green('[API log] json写入成功'))
+            this.filePath = path.resolve(__dirname, output)
+            fs.writeFileSync(this.filePath, JSON.stringify(result, null, 4), {flag: 'w+'})
+            console.log(colors.green('[API log] ', state.opts.sourceFileName, ' 解析完成'))
         }
     }
 }
